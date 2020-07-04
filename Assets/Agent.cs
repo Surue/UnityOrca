efbc2c879128a29cee52e19fbb6542006ff5b9f2
surue <nicolas06062000@hotmail.com>
2020-07-04 22:47:41 +0200

    Add group and find neighbors

---
+++ Assets/Agent.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour {
+    [Header("ORCA")] 
+    [SerializeField] private float radius = 0.5f;
+    [SerializeField] private float timeHorizon_ = 1.0f;
+    [SerializeField] private float neighborsDist_ = 3.0f;
+    private int maxNeighbors = 7; 
+    private List<KeyValuePair<float, Agent>> agentNeighbors_;
+
+    [Header("Movement")]
+    [SerializeField] private float stopDistance = 0.1f;
+    [SerializeField] private float arriveDistance = 1.0f;
+
+    private Rigidbody body_;
+    private Vector3 target_;
+    private Vector3 desiredVelocity_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agentNeighbors_ = new List<KeyValuePair<float, Agent>>();
+        
+        body_ = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check neighbors
+        if (maxNeighbors > 0)
+        {
+            agentNeighbors_.Clear();
+            float rangeSqr = neighborsDist_ * neighborsDist_;
+
+            foreach (var agent in FindObjectsOfType<Agent>())
+            {
+                if (this != agent)
+                {
+                    Vector3 dir = transform.position - agent.transform.position;
+                    float distSqr = Vector3.Dot(dir, dir);
+
+                    //If the other agent is under the minimum range => add it
+                    if (distSqr < rangeSqr)
+                    {
+                        //If there is a free space, add it immediatly
+                        if (agentNeighbors_.Count < maxNeighbors)
+                        {
+                            agentNeighbors_.Add(new KeyValuePair<float, Agent>(distSqr, agent));
+                        }
+                        
+                        //Make sure the list is sorted
+                        int i = agentNeighbors_.Count - 1;
+                        while (i != 0 && distSqr < agentNeighbors_[i - 1].Key)
+                        {
+                            agentNeighbors_[i] = agentNeighbors_[i - 1];
+                            i--;
+                        }
+    
+                        //Once a spot with a further agent is found, place if 
+                        agentNeighbors_[i] = new KeyValuePair<float, Agent>(distSqr, agent);
+
+                        //If the list is full, only check agent nearer than the farrest neighbor.
+                        if (agentNeighbors_.Count == maxNeighbors)
+                        {
+                            rangeSqr = agentNeighbors_[agentNeighbors_.Count - 1].Key;
+                        }
+                    }
+                }
+            }
+        }
+        
+        //Update movement
+        float dist = Vector3.Distance(transform.position, target_);
+
+        if (dist < stopDistance)
+        {
+            desiredVelocity_ = Vector3.zero;
+        }else if (dist < arriveDistance)
+        {
+            desiredVelocity_ = (target_ - transform.position).normalized * 5.0f * (dist / arriveDistance);
+        }
+        else
+        {
+            desiredVelocity_ = (target_ - transform.position).normalized * 5.0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float yVel = body_.velocity.y;
+        body_.velocity = new Vector3(desiredVelocity_.x, yVel, desiredVelocity_.z);
+        transform.forward = new Vector3(desiredVelocity_.x, 0, desiredVelocity_.z);
+    }
+
+    public void SetTarget(Vector3 newTarget)
+    {
+        target_ = newTarget;
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach (var keyValuePair in agentNeighbors_)
+        {
+            Gizmos.DrawLine(transform.position, keyValuePair.Value.transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/GroupFormation.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GroupFormation : MonoBehaviour
+{
+    [Serializable]
+    enum Shape {
+        SQUARE,
+        LINE,
+        COLUMN
+    }
+
+    enum State {
+        FORMING,
+        FORMED
+    }
+
+    [Header("Group settings")] 
+    [SerializeField] private GameObject prefabAgent_;
+    [SerializeField] private Shape shape_;
+    private State state_ = State.FORMING;
+    [SerializeField] private int nbAgent_;
+    private List<Agent> agents_;
+
+    [Header("Movement")] 
+    [SerializeField] private float speedForming_;
+    [SerializeField] private float speedFormed_;
+    [SerializeField] private float agentSpacing_ = 1.5f;
+    [SerializeField] private float avoidanceSpeed_ = 1.5f;
+
+    private List<Vector3> targetsPositionFormation_;
+    private List<Vector3> desiredVelocities_;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetsPositionFormation_ = new List<Vector3>();
+        agents_ = new List<Agent>();
+        desiredVelocities_ = new List<Vector3>();
+        
+        for (int i = 0; i < nbAgent_; i++)
+        {
+            Vector2 randomCircle = Random.insideUnitCircle * 5;
+            Vector3 randomPos = new Vector3(randomCircle.x, 3, randomCircle.y);
+
+            GameObject instance = Instantiate(prefabAgent_, randomPos, Quaternion.identity);
+            agents_.Add(instance.GetComponent<Agent>());
+            
+            targetsPositionFormation_.Add(GetTargetPositionByIndex(i));
+            desiredVelocities_.Add(Vector3.zero);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float speed = 0;
+        
+        //Update target position
+        for (int i = 1; i < nbAgent_; i++)
+        {
+            targetsPositionFormation_[i] = GetTargetPositionByIndex(i);
+        }
+        
+        //reset desired velocities
+        //Update target position
+        for (int i = 0; i < nbAgent_; i++)
+        {
+            desiredVelocities_[i] = Vector3.zero;
+        }
+        
+        switch (state_)
+        {
+            case State.FORMING:
+                for (int i = 0; i < nbAgent_; i++)
+                {
+                    agents_[i].SetTarget(targetsPositionFormation_[i]);
+                }
+                break;
+            case State.FORMED:
+                speed = speedFormed_;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            targetsPositionFormation_[0] = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        
+        //ORCA
+        
+        
+        
+        //Check for obstacle
+        for (int i = 1; i < nbAgent_; i++)
+        {
+            var t = agents_[i].transform;
+            // for (int x = -1; x <= 1; x++)
+            // {
+            //     Debug.DrawLine(t.position + desiredVelocities_[i].normalized * 0.51f + t.right * (x * 0.5f),
+            //         t.position + desiredVelocities_[i].normalized * 0.51f +
+            //         desiredVelocities_[i].normalized * (agentSpacing_ * 0.4f) + t.right * (x * 0.5f));
+            //     
+            //     if (Physics.Raycast(t.position + desiredVelocities_[i].normalized * 0.51f + t.right * (x * 0.5f),
+            //         desiredVelocities_[i].normalized, agentSpacing_ * 0.4f))
+            //     {
+            //         // desiredVelocities_[i] = desiredVelocities_[i] * 0.5f;
+            //         desiredVelocities_[i] += t.right * avoidanceSpeed_;
+            //         // break;
+            //     }
+            // }
+        }
+    }
+
+    Vector3 GetTargetPositionByIndex(int index)
+    {
+        switch (shape_)
+        {
+            case Shape.SQUARE:
+                int maxCol = Mathf.FloorToInt(Mathf.Sqrt(nbAgent_));
+
+                int col = index % maxCol;
+                int row = index / maxCol;
+
+                if (col % 2 == 0)
+                {
+                    return agents_[0].transform.position +
+                           agents_[0].transform.right * (col / 2 * agentSpacing_) -
+                           agents_[0].transform.forward * (row * agentSpacing_);
+                }
+                else
+                {
+                    return agents_[0].transform.position - 
+                           agents_[0].transform.right * (Mathf.CeilToInt(col / 2.0f) * agentSpacing_) -
+                           agents_[0].transform.forward * (row * agentSpacing_);
+                }
+                break;
+            case Shape.LINE:
+                if (index % 2 == 0)
+                {
+                    return agents_[0].transform.position + agents_[0].transform.right * (index / 2.0f * agentSpacing_);
+                }
+                else
+                {
+                    return agents_[0].transform.position - agents_[0].transform.right * (Mathf.CeilToInt(index / 2.0f) * agentSpacing_);
+                }
+            case Shape.COLUMN:
+                return agents_[0].transform.position - agents_[0].transform.forward * (index * agentSpacing_);
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        
+        return Vector3.zero;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (agents_ == null || agents_.Count == 0) return; 
+        
+        switch (shape_)
+        {
+            case Shape.SQUARE:
+                int maxRow = Mathf.CeilToInt(Mathf.Sqrt(nbAgent_));
+                int maxCol = Mathf.FloorToInt(Mathf.Sqrt(nbAgent_));
+                
+                
+                for (int row = 0; row < maxRow; row++)
+                {
+                    for (int col = 0; col < maxCol; col++)
+                    {
+                        if (row * maxCol + col >= nbAgent_)
+                        {
+                            return;
+                        }
+                        
+                        if (col % 2 == 0)
+                        {
+                            Gizmos.DrawWireSphere(agents_[0].transform.position +
+                                                  (agents_[0].transform.right * (col / 2) * agentSpacing_) -
+                                                  (agents_[0].transform.forward * row * agentSpacing_),
+                                0.5f);
+                        }
+                        else
+                        {
+                            Gizmos.DrawWireSphere(agents_[0].transform.position -
+                                                  (agents_[0].transform.right * Mathf.CeilToInt(col / 2.0f) * agentSpacing_) -
+                                                  (agents_[0].transform.forward * row * agentSpacing_),
+                                0.5f);
+                        }
+                    }
+                }
+                break;
+            case Shape.LINE:
+                for (int i = 0; i < nbAgent_; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Gizmos.DrawWireSphere(agents_[0].transform.position + agents_[0].transform.right * (i / 2.0f) * agentSpacing_,
+                            0.5f);
+                    }
+                    else
+                    {
+                        Gizmos.DrawWireSphere(agents_[0].transform.position - agents_[0].transform.right * (Mathf.CeilToInt(i / 2.0f) * agentSpacing_),
+                            0.5f);
+                    }
+                }
+                break;
+            case Shape.COLUMN:
+                for (int i = 0; i < nbAgent_; i++)
+                {
+                    Gizmos.DrawWireSphere(agents_[0].transform.position - agents_[0].transform.forward * i * agentSpacing_, 0.5f);
+                }
+
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
